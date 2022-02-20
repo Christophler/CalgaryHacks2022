cbf059b07efd7bc4a0fb138cab7c49ca2152b697
hoomji <selina99tran@gmail.com>
2022-02-19 20:32:07 -0500

    <docs> added navigation <feat> added navigation node modules

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,8 @@
 
 import React from 'react';
 import {View, Text, Dimensions, TextInput, SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
 import NavigationBar from './components/NavigationBar';
 import generalStyles from './generalStyles';
 
@@ -15,26 +17,30 @@ const win = Dimensions.get('window');
 
 const App = () => {
   return (
-    <View style={{height: win.height - 27}}>
-      <Text style={generalStyles.baseText}>From:</Text>
+    <>
+      <NavigationContainer>
+        <View style={{height: win.height - 27}}>
+          <Text style={generalStyles.baseText}>From:</Text>
 
-      <TextInput
-        style={generalStyles.input}
-        placeholder={'Enter the starting location'}
-      />
+          <TextInput
+            style={generalStyles.input}
+            placeholder={'Enter the starting location'}
+          />
 
-      <Text style={generalStyles.baseText}>
-        <Text style={generalStyles.titleText}>
-          {'Title'}
-          {'\n'}
-          {'\n'}
-        </Text>
-        <Text numberOfLines={5}>{'Hi'}</Text>
-      </Text>
-      <View style={{position: 'absolute', bottom: 0, width: '100%'}}>
-        <NavigationBar></NavigationBar>
-      </View>
-    </View>
+          <Text style={generalStyles.baseText}>
+            <Text style={generalStyles.titleText}>
+              {'Title'}
+              {'\n'}
+              {'\n'}
+            </Text>
+            <Text numberOfLines={5}>{'Hi'}</Text>
+          </Text>
+          <View style={{position: 'absolute', bottom: 0, width: '100%'}}>
+            <NavigationBar></NavigationBar>
+          </View>
+        </View>
+      </NavigationContainer>
+    </>
   );
 };
 
